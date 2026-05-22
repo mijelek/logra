28bb5d55e92756e700331adb757b1d57504a74c0
mijelek <mijelekrizovnik@gmail.com>
2026-05-22 16:16:40 +0100

    Create feeds.py

---
+++ scraper/feeds.py
@@ -0,0 +1,34 @@
+RSS_FEEDS = [
+    {
+        'url': 'https://www.technologyreview.com/feed/',
+        'category': 'progression'
+    },
+    {
+        'url': 'https://openai.com/blog/rss/',
+        'category': 'progression'
+    },
+    {
+        'url': 'https://www.wired.com/feed/tag/artificial-intelligence/rss',
+        'category': 'awareness'
+    },
+    {
+        'url': 'https://feeds.feedburner.com/blogspot/gJZg',  # Google AI Blog
+        'category': 'progression'
+    },
+    {
+        'url': 'https://towardsdatascience.com/feed',
+        'category': 'awareness'
+    },
+    {
+        'url': 'https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml',
+        'category': 'awareness'
+    },
+    {
+        'url': 'https://venturebeat.com/category/ai/feed/',
+        'category': 'progression'
+    },
+    {
+        'url': 'https://www.theverge.com/rss/ai-artificial-intelligence/index.xml',
+        'category': 'awareness'
+    }
+]
